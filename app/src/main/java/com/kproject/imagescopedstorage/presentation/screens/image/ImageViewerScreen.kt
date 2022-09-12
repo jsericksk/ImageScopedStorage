59100d85b81d52adaeee3607dc4b9cf4725e8553
jsericksk <jsericksk@gmail.com>
2022-09-12 20:08:30 -0300

    project: Small improvements

--- app/src/main/java/com/kproject/imagescopedstorage/presentation/screens/image/ImageViewerScreen.kt
+++ app/src/main/java/com/kproject/imagescopedstorage/presentation/screens/image/ImageViewerScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +29,6 @@ import com.kproject.imagescopedstorage.presentation.screens.components.AlertDial
 import com.kproject.imagescopedstorage.presentation.screens.components.CustomImage
 import com.kproject.imagescopedstorage.presentation.screens.components.TopBar
 import com.kproject.imagescopedstorage.presentation.theme.ImageScopedStorageTheme
-import java.lang.IndexOutOfBoundsException
 
 @Composable
 fun ImageViewerScreen(
